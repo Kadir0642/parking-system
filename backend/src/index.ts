4bf4594aaa40686bcf4f3c0ba757d8f89419d0c7
Kadir0642 <kadirk061942@gmail.com>
2026-01-28 18:24:34 +0300

    feat: proje baslangici ve paket kurulumlari

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+// npx ts-node src/index.ts
+// Node.js, benim için src klasöründeki index.ts dosyasını 
+// TypeScript olarak çalıştır
+
+console.log("Merhaba BİLPARK! ...");
+
+const multiply=(num1:number,num2:number):number=>{
+    return num1*num2;
+}
+
+console.log("Test çarpimi "+ multiply(11,12));
+
