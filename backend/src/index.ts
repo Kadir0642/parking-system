c7df484947aca96c3fb11f4f0cfd81bc75dc8a2e
Kadir0642 <kadirk061942@gmail.com>
2026-01-29 14:42:10 +0300

    fet: İlk sunucu ayağa kaldırma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,32 @@
-// npx ts-node src/index.ts
-// Node.js, benim için src klasöründeki index.ts dosyasını 
-// TypeScript olarak çalıştır
+/* Artık "npm run dev" ile çalışır.
+* "dev":"nodemon src/index.ts" sayesinde (package.json/scripts)
+* nodemon devreye girecek ve her kayıt alışta 
+* sunucuyu otomatik yeniden başlatacak.
+*/
+import express, {Request, Response } from 'express';
 
-console.log("Merhaba BİLPARK! ...");
+import dotenv from 'dotenv';
 
-const multiply=(num1:number,num2:number):number=>{
-    return num1*num2;
-}
+//1.ayarları yükle (.env dosyasını okur)
+dotenv.config();
 
-console.log("Test çarpimi "+ multiply(11,12));
+//2.Uygulamayı (Server) oluştur.
+const app =express(); // bana boş bir sunucu binası inşa et
+
+//Eğer .env dosyasında PORT yoksa 3000'i kullan
+const PORT=process.env.PORT || 3000;
+
+// 3.bir "Rota" (Route) Tanımla
+//Tarayıcıdan ana sayfaya "localhost:3000/" gelindiğinde ne olsun ?
+
+//Birisi kapıdan ( ana adres / )girerse,ona "... mesajı göster"
+app.get('/',(req: Request, res:Response)=> {
+    res.send('Bilpark Backend Servisi Çalişiyor! ');
+});
+
+//4.Sunucuyu Başlat ve Dinlemeye Başla
+app.listen(PORT, ()=>{
+    console.log(`Sunucu şu adreste çalişiyor: http://localhost:${PORT}`);
+})
+//3000 numaralı Port için dinlemeye al
 
